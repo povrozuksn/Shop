cc16f4d5890424daf42d6bfe9b4cfb9f5145065c
povser75@outlook.com <povser75@outlook.com>
2025-01-24 18:27:59 +0400

    Создание объекта через структуру, список объектов

--- Shop/MainForm.cs
+++ Shop/MainForm.cs
@@ -10,8 +10,35 @@ using System.Windows.Forms;
 
 namespace Shop
 {
+    public struct Product
+    {
+        public string name;
+        public string category;
+        public int сalorie;
+        public int period;
+        public int price;
+        public PictureBox picture;
+        public Label label;
+
+        public Product(string _name, string _category, int _сalorie, int _period, int _price)
+        {
+            name = _name;
+            category = _category;
+            сalorie = _сalorie;
+            period = _period;
+            price = _price;
+            picture = new PictureBox();
+            picture.Load("../../Pictures/" + name + ".jpg");
+            label = new Label();
+            label.Text = name;
+        }
+    }
+
+    
     public partial class MainForm : Form
     {
+        List<Product> products = new List<Product>();
+
         public MainForm()
         {
             InitializeComponent();
@@ -19,6 +46,13 @@ namespace Shop
             NameLabel.Visible = false;
             FiltrPanel.Height = HideButton.Height;
             HideButton.Text = "Развернуть";
+
+            products[0] = new Product("Хлеб ржаной", "Хлебобулочные изделия", 259, 36, 30);
+            products[1] = new Product("Хлеб белый", "Хлебобулочные изделия", 265, 24, 40);
+            products[2] = new Product("Молоко 32%", "Кисломолочные продукты", 64, 7, 90);
+            products[3] = new Product("Кефир", "Кисломолочные продукты", 53, 14, 70);
+            products[4] = new Product("Колбаса", "Мясные продукты", 257, 7, 170);
+
         }
 
         private void picProduct_Click(object sender, EventArgs e)
