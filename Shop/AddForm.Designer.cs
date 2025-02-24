e2cc65fe7398095fd7d8bb322524f5650a0d5c1d
povser75@outlook.com <povser75@outlook.com>
2025-02-24 18:31:22 +0400

    Добавление объекта

--- Shop/AddForm.Designer.cs
+++ Shop/AddForm.Designer.cs
@@ -28,21 +28,261 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.NameTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.CategoryComboBox = new System.Windows.Forms.ComboBox();
+            this.KallTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.PeriodTextBox = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.PriceTextBox = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.AddButton = new System.Windows.Forms.Button();
+            this.label10 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.LoadPicButton = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.OpisTextBox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(336, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Добавление объекта в стравочник";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(231, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Введите наименование";
+            // 
+            // NameTextBox
+            // 
+            this.NameTextBox.Location = new System.Drawing.Point(249, 48);
+            this.NameTextBox.Name = "NameTextBox";
+            this.NameTextBox.Size = new System.Drawing.Size(249, 30);
+            this.NameTextBox.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 89);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(209, 25);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Выберите категорию";
+            // 
+            // CategoryComboBox
+            // 
+            this.CategoryComboBox.FormattingEnabled = true;
+            this.CategoryComboBox.Items.AddRange(new object[] {
+            "",
+            "Хлебобулочные изделия",
+            "Кисломолочные продукты",
+            "Мясные продукты",
+            "Фрукты",
+            "Овощи"});
+            this.CategoryComboBox.Location = new System.Drawing.Point(249, 86);
+            this.CategoryComboBox.Name = "CategoryComboBox";
+            this.CategoryComboBox.Size = new System.Drawing.Size(249, 33);
+            this.CategoryComboBox.TabIndex = 4;
+            // 
+            // KallTextBox
+            // 
+            this.KallTextBox.Location = new System.Drawing.Point(249, 129);
+            this.KallTextBox.Name = "KallTextBox";
+            this.KallTextBox.Size = new System.Drawing.Size(130, 30);
+            this.KallTextBox.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 132);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(226, 25);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Введите калорийность";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(385, 132);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(68, 25);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Ккалл";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(385, 175);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(48, 25);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "сут.";
+            // 
+            // PeriodTextBox
+            // 
+            this.PeriodTextBox.Location = new System.Drawing.Point(249, 172);
+            this.PeriodTextBox.Name = "PeriodTextBox";
+            this.PeriodTextBox.Size = new System.Drawing.Size(130, 30);
+            this.PeriodTextBox.TabIndex = 9;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 175);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(229, 25);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Введите срок годности";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(385, 214);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(48, 25);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "руб.";
+            // 
+            // PriceTextBox
+            // 
+            this.PriceTextBox.Location = new System.Drawing.Point(249, 214);
+            this.PriceTextBox.Name = "PriceTextBox";
+            this.PriceTextBox.Size = new System.Drawing.Size(130, 30);
+            this.PriceTextBox.TabIndex = 12;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(12, 214);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(200, 25);
+            this.label9.TabIndex = 11;
+            this.label9.Text = "Введите стоимость";
+            // 
+            // AddButton
+            // 
+            this.AddButton.Location = new System.Drawing.Point(12, 349);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(177, 49);
+            this.AddButton.TabIndex = 14;
+            this.AddButton.Text = "Добавить";
+            this.AddButton.UseVisualStyleBackColor = true;
+            this.AddButton.Click += new System.EventHandler(this.AddButton_Click);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label10.ForeColor = System.Drawing.Color.Red;
+            this.label10.Location = new System.Drawing.Point(13, 264);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(355, 20);
+            this.label10.TabIndex = 15;
+            this.label10.Text = "Все поля обязательны к заполнению";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(518, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(293, 227);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 16;
+            this.pictureBox1.TabStop = false;
+            // 
+            // LoadPicButton
+            // 
+            this.LoadPicButton.Location = new System.Drawing.Point(518, 240);
+            this.LoadPicButton.Name = "LoadPicButton";
+            this.LoadPicButton.Size = new System.Drawing.Size(293, 33);
+            this.LoadPicButton.TabIndex = 17;
+            this.LoadPicButton.Text = "Загрузить картинку";
+            this.LoadPicButton.UseVisualStyleBackColor = true;
+            this.LoadPicButton.Click += new System.EventHandler(this.LoadPicButton_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // OpisTextBox
+            // 
+            this.OpisTextBox.Location = new System.Drawing.Point(833, 12);
+            this.OpisTextBox.Multiline = true;
+            this.OpisTextBox.Name = "OpisTextBox";
+            this.OpisTextBox.Size = new System.Drawing.Size(404, 385);
+            this.OpisTextBox.TabIndex = 18;
             // 
             // AddForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(726, 473);
+            this.ClientSize = new System.Drawing.Size(1253, 410);
+            this.Controls.Add(this.OpisTextBox);
+            this.Controls.Add(this.LoadPicButton);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.AddButton);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.PriceTextBox);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.PeriodTextBox);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.KallTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.CategoryComboBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.NameTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "AddForm";
             this.Text = "AddForm";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox NameTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox CategoryComboBox;
+        private System.Windows.Forms.TextBox KallTextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox PeriodTextBox;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox PriceTextBox;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button AddButton;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button LoadPicButton;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox OpisTextBox;
     }
 }
--- Shop/AddForm.cs
+++ Shop/AddForm.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,50 @@ namespace Shop
         public AddForm()
         {
             InitializeComponent();
+        }
+
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+            int a;
+            if(!Int32.TryParse(KallTextBox.Text, out a) || !Int32.TryParse(PeriodTextBox.Text, out a) || !Int32.TryParse(PriceTextBox.Text, out a))
+            {
+                MessageBox.Show("Не число");
+                return;
+            }
+
+            if(NameTextBox.Text=="" || CategoryComboBox.Text=="" || KallTextBox.Text=="" || PeriodTextBox.Text=="" || PriceTextBox.Text=="")
+            {
+                MessageBox.Show("Все поля обязательны к заполнению");
+                return;
+            }
+
+            System.IO.File.AppendAllText("../../Pictures/products.txt", Environment.NewLine +
+                                                                        NameTextBox.Text + ", " +
+                                                                        CategoryComboBox.Text + ", " +
+                                                                        KallTextBox.Text + ", " +
+                                                                        PeriodTextBox.Text + ", " + 
+                                                                        PriceTextBox.Text);
+
+
+            if(FileName != "")
+            {
+                System.IO.File.Copy(FileName, "../../Pictures/" + NameTextBox.Text + ".jpg");
+            }
+
+            System.IO.File.AppendAllText("../../Pictures/" + NameTextBox.Text + ".txt", OpisTextBox.Text);
+
+            MessageBox.Show("Сорханено");
+            Close();
+        }
+
+        string FileName = "";
+        private void LoadPicButton_Click(object sender, EventArgs e)
+        {
+            if(openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                FileName = openFileDialog1.FileName;
+                pictureBox1.Load(FileName);
+            }
         }
     }
 }
